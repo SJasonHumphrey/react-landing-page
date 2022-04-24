aaf6b9a084f4b38cbcdbf70e4784243d38434cd6
SJasonHumphrey <sjasonhumphrey@gmail.com>
2022-04-24 13:52:20 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import saintlucis from './assets/saintlucia.jpeg';
 import beachDock from './assets/beachDock.jpeg';
 import Hero from './components/Hero';
 import { Slider } from './components/Slider';
+import Navbar from './components/Navbar';
 
 
 function App() {
+  const navbarLinks = [
+    { url: "#", title: "Home" },
+    { url: "#", title: "Trips" },
+    { url: "#", title: "Rewards" },
+  ];
   return (
     <div className='App'>
+      <Navbar navbarLinks={navbarLinks}/>
       <Hero imageSrc={beach}/>
-      <Slider imageSrc={vacation} title={"Title"} subtitle={"subtitle"} />
-      <Slider imageSrc={beachDock} title={"Title"} subtitle={"subtitle"} flipped={true}/>
+      <Slider imageSrc={vacation} title={"Relax"} subtitle={"subtitle"} />
+      <Slider imageSrc={beachDock} title={"Explore"} subtitle={"subtitle"} flipped={true}/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,41 @@
-import React from "react";
-import "./Navbar.css"
-import {FiMenu, FiX} from "react-icons/fi"
+import React, { useState } from "react";
+import "./Navbar.css";
+import { FiMenu, FiX } from "react-icons/fi";
 
+const Navbar = ({ navbarLinks }) => {
+  const [menuClicked, setMenuClicked] = useState(false);
 
-const Navbar = ({navbarLinks}) => {
+  const toggleMenuClick = () => {
+    setMenuClicked(!menuClicked);
+  };
   return (
-  <nav className="navbar">
-    <span className="navbar_logo">Travel</span>
-      <ul className="navbar_list">{navbarLinks.map(item => {
-        return(
-          <li className="navbar_item" key={item.title}>
-            <a className="navbar_link" href={item.url}>
-              {item.title}
+    <nav className="navbar">
+      <span className="navbar_logo">myTravel</span>
+      {menuClicked ? (
+        <FiX size={25} className={"navbar_menu"} onClick={toggleMenuClick} />
+      ) : (
+        <FiMenu
+          size={25}
+          className={"navbar_menu"}
+          onClick={toggleMenuClick}
+        />
+      )}
+      <ul
+        className={
+          menuClicked ? "navbar_list navbar_list--active" : "navbar_list"
+        }
+      >
+        {navbarLinks.map((item) => {
+          return (
+            <li className="navbar_item" key={item.title}>
+              <a className="navbar_link" href={item.url}>
+                {item.title}
               </a>
-          </li>
-        );
-      })}
+            </li>
+          );
+        })}
       </ul>
-  </nav>
+    </nav>
   );
 };
 
